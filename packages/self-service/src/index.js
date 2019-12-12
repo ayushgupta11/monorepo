3d54755e71523c00a86fa30b96e9ff592abb9221
ayush_1197 <ayush.gupta.1197@gmail.com>
2019-12-12 11:18:59 +0530

    Minor fixes

--- packages/self-service/src/index.js
+++ packages/self-service/src/index.js
@@ -1,9 +1,12 @@
 import express from 'express'
 import bodyParser from 'body-parser'
 import cors from 'cors'
+import { db } from '@monorepo/shared'
 
 const app = express()
 const port = process.env.PORT || 8080
+
+console.log(db)
 app.use(bodyParser.json())
 app.use(cors())
 
--- packages/shared/src/index.js
+++ packages/shared/src/index.js
@@ -1,3 +1,3 @@
 import db from './helpers/mongodb'
 
-export default { db }
+export  { db }
